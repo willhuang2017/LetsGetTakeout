ab8330ecd60388a99f0da7352a2f24b00da6c659
willhuang2017 <williamhuang2017@gmail.com>
2023-05-02 20:59:49 -0400

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,27 @@
+// Create Customer class here
+public class Customer {
+
+  private String name;
+  private int money;
+
+  public Customer(String name, int money){
+    this.name = name;
+    this.money = money;
+  }
+
+  public void setName(String name){
+    this.name = name;
+  }
+
+  public void setMoney(int money){
+    this.money = money;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+  public int getMoney(){
+    return this.money;
+  }
+}
