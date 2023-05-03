b8ffb5690e49053eaa000be0f9471d7a70bc603a
willhuang2017 <williamhuang2017@gmail.com>
2023-05-02 20:59:36 -0400

    Create Food.java

---
+++ Food.java
@@ -0,0 +1,27 @@
+// Create Food class here
+public class Food implements PricedItem<Integer>{
+  private String name;
+  private String description;
+  private int price;
+
+  public Food(String name, String description, int price){
+    this.name = name;
+    this.description = description;
+    this.price = price;
+  }
+
+  @Override
+  public void setPrice(Integer price){
+    this.price = price;
+  }
+
+  @Override
+  public Integer getPrice(){
+    return this.price;
+  }
+
+  @Override
+  public String toString(){
+    return ("Enjoy "+this.name+": "+this.description+"    Cost: $" + this.price);
+  }
+}
